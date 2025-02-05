a176601e49034f616bab1e7ba53d61e5207def80
wgsylcl <2116766237@qq.com>
2025-02-05 11:31:04 +0800

    upd

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 cargo install mdbook
+cargo install mdbook-toc
 cd mdbook-mathjax-preprocessor
 cargo build --release
 cd ..
