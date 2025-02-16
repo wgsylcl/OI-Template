1dc241ca60327305a3c377e5c970bccf3ce691f3
wgsylcl <2116766237@qq.com>
2025-02-16 18:05:22 +0800

    upd

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 cargo install mdbook
+cargo install mdbook-katex
 cargo install mdbook-admonish
 mdbook-admonish install
 python runbook.py build
