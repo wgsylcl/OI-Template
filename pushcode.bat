f6f9f6d30662bb2fcf75ed222f2c064b7d47cf7d
wgsylcl <2116766237@qq.com>
2025-02-22 11:34:51 +0800

    update ac

--- pushcode.bat
+++
@@ -1,4 +0,0 @@
-git pull
-git add %~dp0
-git commit %~dp0 -m "upd"
-git push
--- pushcode.ps1
+++
@@ -1,9 +0,0 @@
-while ($true) {
-    $ps1FilePath = $MyInvocation.MyCommand.Definition
-    $scriptDir = Split-Path $ps1FilePath
-    git pull
-    git add $scriptDir
-    git commit $scriptDir -m "upd"
-    git push
-    timeout /t 900
-}
