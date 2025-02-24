736627335ef709c00a955ae68420a051f7cc1dc1
wgsylcl <2116766237@qq.com>
2025-02-24 13:05:51 +0800

    upd

--- configure.sh
+++ configure.sh
@@ -1,6 +1,5 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-npm install -D netlify-plugin-rust-cache
 cargo install mdbook mdbook-katex mdbook-admonish mdbook-embedify --jobs=4
 mdbook-admonish install
 mdbook build
